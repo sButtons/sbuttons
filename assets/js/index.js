0bc3856fa57e763a032291ec11408b33d895f45d
shahed.nasser <shahed.nasser@lurner.com>
2020-09-01 12:29:17 +0300

    added links for submenus

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,9 +21,22 @@ $(document).ready(function(){
   function checkActiveCategory(){
     $("section").each(function(){
       if($(this).isOnScreen()){
+        var sidebarLink = $(".sidebar a[href='#" + $(this).attr('id') + "']");
         $(".sidebar a.active").removeClass("active");
-        $(".sidebar a[href='#" + $(this).attr('id') + "']").addClass("active");
-        return false;
+        sidebarLink.addClass("active");
+        var hasClass = sidebarLink.hasClass('submenu-link'),
+            nextHasClass = sidebarLink.next().hasClass('submenu-links');
+        if (hasClass || (!hasClass && !nextHasClass)) {
+          var parent = sidebarLink.parent(),
+              hasClass = parent.hasClass('submenu-links');
+          if (hasClass && !parent.hasClass('show')) {
+            $(".submenu-links.show").removeClass('show');
+            parent.addClass('show');
+          } else if (!hasClass) {
+            $(".submenu-links.show").removeClass('show');
+          }
+          return false;
+        }
       }
     });
   }
