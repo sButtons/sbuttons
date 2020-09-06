3e219e6038be369c79704ab8731602c9b9c5d217
Pratyush Saxema <devil_geek@yahoo.com>
2020-09-06 15:35:20 +0530

    scroll-top btn added

--- assets/js/index.js
+++ assets/js/index.js
@@ -60,4 +60,25 @@ $(document).ready(function(){
     checkNavbar();
     checkActiveCategory();
   });
+
+  
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scroll-top').fadeIn();
+    } else {
+      $('.scroll-top').fadeOut();
+    }
+  });
+
+  $('.scroll-top').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 100);
+      return false;
+  });
+
+
+
 });
+
+
