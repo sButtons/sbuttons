4bd52608d4690db666155711b4abbb528132f21b
Zuhdan Nur <pczuhdan33@gmail.com>
2020-09-27 17:34:39 +0700

    Hide Download button in the navbar when at the top of the page , using css

--- assets/js/index.js
+++ assets/js/index.js
@@ -51,12 +51,9 @@ $(document).ready(function () {
     if (!$(".content").isOnScreen()) {
       $(".navbar").addClass("scrolling");
       $(".sidebar").addClass("scrolling");
-      $(".downloadIcon").css('display','none')
     } else {
       $(".downloadIcon").css('display','inline-block')      
-      $(".navbar").removeClass("scrolling");
-      $(".sidebar").removeClass("scrolling");
-    
+      $(".navbar").removeClass("scrolling");    
     }
   }
 
