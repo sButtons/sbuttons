e10a342c51fc5db89d235ab0fe7e1447f352dcf5
vishakhaFincity <vishakha.awasthi@fincity.com>
2020-10-02 04:15:29 +0530

    Resolved Conflicts

--- assets/js/buttons-examples.js
+++ assets/js/buttons-examples.js
@@ -129,6 +129,12 @@ $(document).ready(function () {
       ],
     },
     {
+      type: "dashed",
+      label: "Dashed Buttons",
+      classes: "dashed-btn",
+      children: [],
+    },
+    {
       type: "disable",
       classes: "disabled-btn",
       label: "Disabled Buttons",
@@ -187,6 +193,12 @@ $(document).ready(function () {
           classes: "plus-btn",
           text: "",
         },
+        {
+          type: "scroll-to-top",
+          label: "Scroll-to-Top Button",
+          classes: "scroll-to-top-btn",
+          text: "",
+        },
       ],
     },
     {
@@ -209,6 +221,10 @@ $(document).ready(function () {
           text: "Login with Facebook",
         },
         {
+          classes: "flickr",
+          text: "Login with Flickr",
+        },
+        {
           classes: "github",
           text: "Login with Github",
         },
@@ -225,8 +241,16 @@ $(document).ready(function () {
           text: "Login with LinkedIn",
         },
         {
+          classes: "snapchat",
+          text: "Login with Snapchat",
+        },
+        {
           classes: "twitter",
           text: "Login with Twitter",
+        },
+        {
+          classes: "tumblr",
+          text: "Login with Tumblr",
         },
         {
           classes: "weibo",
@@ -241,6 +265,38 @@ $(document).ready(function () {
       classes: "dropdown-btn",
       children: [],
       text: "Dropdown",
+    },
+    {
+      type: "special",
+      label: "Special Buttons",
+      classes: "",
+      children: [
+        {
+          type: "play-game",
+          label: "Play game Button",
+          classes: "play-game-btn",
+          text: "",
+        },
+        {
+          type: "retro",
+          label: "Retro",
+          classes: "retro-btn",
+          variations: [
+            {
+              classes: "",
+              text: "Continue",
+            },
+            {
+              classes: "retro-btn-proceed",
+              text: "Proceed",
+            },
+            {
+              classes: "retro-btn-reset",
+              text: "Reset",
+            },
+          ],
+        },
+      ],
     },
   ];
 
@@ -380,6 +436,6 @@ $(document).ready(function () {
 
   for (var i = 0; i < buttons.length; i++) {
     var section = createSection(buttons[i], true);
-    section.appendTo(content).before('<hr class="secondary-hr">');
+    section.appendTo(content);
   }
 });
