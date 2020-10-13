8956c0fd6b4e9d76e3d706aeab16eb7aa6cd7d56
shahednasser <shahednasser@users.noreply.github.com>
2020-10-13 19:32:55 +0000

    Apply prettier changes

--- assets/js/buttons-examples.js
+++ assets/js/buttons-examples.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
           type: "click",
           label: "Click",
           classes: "click-btn",
-        },        
+        },
         {
           type: "dashed",
           label: "Dashed Buttons",
