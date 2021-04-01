39933384a87936d4a221a968449ca804dbc175c6
snapecode <smasnape@gmail.com>
2021-04-01 02:43:01 -0400

    Fixed Sidebar Link level

--- website/src/components/SidebarLink.js
+++ website/src/components/SidebarLink.js
@@ -31,7 +31,7 @@ function SidebarLink ({text, to = "", level = 0, clickCallback = null, current =
     return (
         <Link to={to.length ? to : '#'} 
             onClick={scroll}
-            className={`block ${active ? 'border-red-600' : ''} sidebar-item lg:w-max text-lg border-b-2 border-transparent hover:border-red-600 dark:text-white ml-${level ? level + 3 : 0}`}>{text}</Link>
+            className={`block ${active ? 'border-red-600' : ''} sidebar-item lg:w-max text-lg border-b-2 border-transparent hover:border-red-600 dark:text-white ml-${level ? level + 2 : 0}`}>{text}</Link>
     )
 }
 
