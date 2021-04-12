3727ce55f5ee3b5fe138507e4e98afc4da292df3
kcbelford <kcbelford@hotmail.com>
2021-04-12 08:18:53 -0700

    adjust height of sidebar and add bottom border

--- website/src/components/Sidebar.js
+++ website/src/components/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Sidebar ({children = []}) {
     return (
         <div className="md:relative fixed animate__animated -left-full md:left-0 dark:bg-gray-700 bg-white shadow-md h-full md:block md:pt-0 md:w-4/12 top-0 w-9/12 z-20" id="sidebar">
-            <div className="md:fixed h-full md:h-5/6 md:pt-0 md:w-4/12 overflow-y-auto pb-3 pl-5 pt-5">
+            <div className="md:fixed h-full md:h-4/6 md:pt-0 md:w-4/12 overflow-y-auto pb-3 pl-5 pt-5 border-b-2 border-gray-100 dark:border-gray-600">
                 {children}
             </div>
         </div>
