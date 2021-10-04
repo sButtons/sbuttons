6f892dcc48f96a41b03dec56fda9949d555b8751
Jared Rigby <jared@mtddigital.uk>
2021-10-04 17:08:47 +0800

    Updated tw classes and markup based on feedback from @shahednasser

--- website/src/components/Sidebar.js
+++ website/src/components/Sidebar.js
@@ -7,12 +7,12 @@ function Sidebar ({children = []}) {
         <div className="md:relative fixed animate__animated -left-full md:left-0 dark:bg-gray-700 bg-white shadow-md h-full md:block md:pt-0 md:w-4/12 top-0 w-9/12 z-20" id="sidebar">
             <div className="md:fixed h-full md:h-4/6 md:pt-0 md:w-4/12 overflow-y-auto pb-3 pl-5 pt-5">
                 <div className="flex pb-5">
-                    <label for="search" className="">Search:</label>
                     <input 
                         name="search" type="text"
                         value={filter}
                         onChange={(e) => setFilter(e.target.value)}
-                        className="ml-2 border-2 border-gray-400 focus:border-red-600"></input>
+                        placeholder="Search"
+                        className="border focus:border-red-600 focus:outline-none pl-3"></input>
                     <button
                         onClick={() => {setFilter("")}} 
                         className={ !filter ? "disabled-btn ml-2 sbtn dashed-btn white-btn xsmall-btn" : "ml-2 sbtn dashed-btn red-btn xsmall-btn"} >Reset</button>
